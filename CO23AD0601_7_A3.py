e31a7924455182c3961562683921d7efc1580b98
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-17 13:29:14 +0530

    Create CO23AD0601_7_A3.py

---
+++ CO23AD0601_7_A3.py
@@ -0,0 +1,7 @@
+total_questions = 12
+max_marks = total_questions * 2
+print("Maximum marks = ", max_marks)
+print("Boolean value = ", bool(max_marks))
+min_marks = total_questions * (-1)
+print("Minimum marks = ", min_marks)
+print("Boolean value = ", bool(min_marks))
